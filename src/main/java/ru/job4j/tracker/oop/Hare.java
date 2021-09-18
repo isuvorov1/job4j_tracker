768a1a559555036c0076b8a421fd11cdc21489e3
isuvorov1 <isuvorov1@outlook.com>
2021-09-18 09:29:37 -0400

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/tracker/oop/Hare.java
@@ -0,0 +1,7 @@
+package ru.job4j.tracker.oop;
+
+public class Hare {
+    void tryEat(Ball ball) {
+
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/oop/Wolf.java
@@ -0,0 +1,7 @@
+package ru.job4j.tracker.oop;
+
+public class Wolf {
+    void tryEat(Ball ball) {
+
+    }
+}
