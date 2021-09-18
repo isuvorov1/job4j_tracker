98285672bd12a4e70cc464a70950b0125e9c59d5
isuvorov1 <isuvorov1@outlook.com>
2021-09-18 09:18:30 -0400

    исправил

--- src/main/java/ru/job4j/tracker/oop/DummyDic.java
+++ src/main/java/ru/job4j/tracker/oop/DummyDic.java
@@ -2,7 +2,6 @@ package ru.job4j.tracker.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-        String rsl = "неизвестное слово ";
-        return rsl + eng;
+        return "неизвестное слово " + eng;
     }
 }
