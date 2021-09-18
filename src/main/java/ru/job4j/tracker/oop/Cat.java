add5fdebcd8fe353dab8a81bf663b92c909f6c02
isuvorov1 <isuvorov1@outlook.com>
2021-09-18 09:17:40 -0400

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/tracker/oop/Cat.java
+++ src/main/java/ru/job4j/tracker/oop/Cat.java
@@ -1,8 +1,29 @@
 package ru.job4j.tracker.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name + " " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat gav = new Cat();
+        gav.giveNick("gav");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("black");
+        black.eat("fish");
+        black.show();
     }
 }
