1102af5a9c71069fc703b779bc431331c1e42943
isuvorov1 <isuvorov1@outlook.com>
2021-09-19 15:14:01 -0400

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/tracker/oop/Battery.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker.oop;
+
+public class Battery {
+    private int load;
+
+    public void exchange(Battery another) {
+        another.load = this.load;
+        this.load -= load;
+    }
+}
