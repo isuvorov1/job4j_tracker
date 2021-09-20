c4efccba2cb646fd636d4203ac1f1b88cd7b83aa
isuvorov1 <isuvorov1@outlook.com>
2021-09-20 06:25:28 -0400

    добавил тесты

---
+++ src/test/java/ru/job4j/tracker/oop/PointTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.tracker.oop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+   @Test
+    public void when00to20then2() {
+        double expected = 2;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
+    }
+
+    @Test
+    public void when10to20then1() {
+        double expected = 1;
+        Point a = new Point(1, 0);
+        Point b = new Point(2, 0);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
+    }
+
+    @Test
+    public void when20to21then1() {
+        double expected = 1;
+        Point a = new Point(2, 0);
+        Point b = new Point(2, 1);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
+    }
+}
+
