91f292d1c14553f86b71fbdaaae0d96e79a6167e
isuvorov1 <isuvorov1@outlook.com>
2021-09-27 17:50:39 -0400

    3.1. Приведение типов. Повышение типа и понижение типа. [#181322]

--- src/main/java/ru/job4j/tracker/oop/College.java
+++ src/main/java/ru/job4j/tracker/oop/College.java
@@ -2,10 +2,8 @@ package ru.job4j.tracker.oop;
 
 public class College {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.setDate("01/09/2021");
-        student.setGroup(21);
-        student.setName("Anton Ivanov");
-        System.out.println("Student name: "+student.getName() + ", student group: "+student.getGroup()+ ", student admission date: " +student.getDate());
+    Freshman vasya = new Freshman();
+    Student st = vasya;
+    Object obj = st;
     }
 }
