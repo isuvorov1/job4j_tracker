a854874d578f7f319b8853ac18432948254c8afe
isuvorov1 <isuvorov1@outlook.com>
2021-10-23 17:19:06 -0400

    0.5. Иерархия исключений и множественный catch. [#219368 #219347]

--- src/main/java/ru/job4j/tracker/ex/UserStore.java
+++ src/main/java/ru/job4j/tracker/ex/UserStore.java
@@ -1,27 +1,19 @@
 package ru.job4j.tracker.ex;
 
 public class UserStore {
-    public static User findUser(User[] users, String login) throws UserNotFoundException {
-        try {
+   public static User findUser(User[] users, String login) throws UserNotFoundException {
             for (User user : users) {
                if (user.getUsername().equals(login)) {
                    return user;
                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } return null;
-    }
+            } return null;
+        }
+
     public static boolean validate(User user) throws UserInvalidException {
-        try {
             if (user.isValid() || user.getUsername().length() >= 3) {
                 return true;
-            }
-        } catch (Exception e){
-            e.printStackTrace();
+            } return false;
         }
-        return false;
-    }
 
     public static void main(String[] args) {
         User[] users = {
