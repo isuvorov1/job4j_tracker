efa513d491fcf4da7dba596fe8417f1264ac4d40
isuvorov1 <isuvorov1@outlook.com>
2021-10-24 11:42:46 -0400

    добавил тест

--- src/test/java/ru/job4j/tracker/collection/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/tracker/collection/PhoneDictionaryTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import java.util.ArrayList;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import org.hamcrest.core.IsNull;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertTrue;
 
 public class PhoneDictionaryTest {
     @Test
@@ -15,5 +18,15 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenNothingFound() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Ivan");
+        assertTrue(persons.isEmpty());
     }
 }
